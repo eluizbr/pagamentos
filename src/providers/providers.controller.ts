132b28eeeb0a38ac2d8e5d11e6fb69e0c62f02fc
Emerson Luiz <eluizbr@gmail.com>
2021-09-28 15:01:53 -0300

    Feat: Documentação Providers

--- src/providers/providers.controller.ts
+++ src/providers/providers.controller.ts
@@ -1,33 +1,93 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { ProvidersService } from './providers.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiBody,
+  ApiCreatedResponse,
+  ApiForbiddenResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateProviderDto } from './dto/create-provider.dto';
 import { UpdateProviderDto } from './dto/update-provider.dto';
+import { Provider } from './entities/provider.entity';
+import { ProvidersService } from './providers.service';
 
+@ApiBearerAuth()
+@ApiTags('Providers')
 @Controller('providers')
+@UseGuards(AuthGuard())
 export class ProvidersController {
   constructor(private readonly providersService: ProvidersService) {}
 
   @Post()
+  @ApiCreatedResponse({
+    description: 'Provider criado com sucesso',
+    type: Provider,
+  })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  @ApiBody({ type: Provider })
   create(@Body() createProviderDto: CreateProviderDto) {
     return this.providersService.create(createProviderDto);
   }
 
   @Get()
+  @ApiOkResponse({
+    description: 'Dados de todos os providers do perfil',
+    type: Provider,
+    isArray: true,
+  })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  @ApiOperation({ summary: 'Retorna todos os providers' })
   findAll() {
     return this.providersService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Retorna o provider pelo ID' })
+  @ApiOkResponse({
+    description: 'Dados do provider',
+    type: Provider,
+  })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
   findOne(@Param('id') id: string) {
     return this.providersService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProviderDto: UpdateProviderDto) {
+  @ApiOperation({ summary: 'Atualiza o provider pelo ID' })
+  @ApiOkResponse({
+    description: 'Dados do provider',
+    type: Provider,
+  })
+  @ApiBody({ type: Provider })
+  @ApiBadRequestResponse({ description: 'Bad Request' })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  update(
+    @Param('id') id: string,
+    @Body() updateProviderDto: UpdateProviderDto,
+  ) {
     return this.providersService.update(+id, updateProviderDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Remove um provider pelo ID' })
+  @ApiOkResponse({ description: 'Provider removido com sucesso' })
+  @ApiForbiddenResponse({ description: 'Forbidden.' })
+  @ApiNotFoundResponse({ description: 'Provider not found' })
   remove(@Param('id') id: string) {
     return this.providersService.remove(+id);
   }
