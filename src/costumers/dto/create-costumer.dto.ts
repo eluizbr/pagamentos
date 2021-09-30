bbd5ca85f1793e2c14e10e9abc724b1f4743499e
Emerson Luiz <eluizbr@gmail.com>
2021-09-29 21:24:56 -0300

    feat: ✨ Costumer
    CreateCostumerDto com validações

--- src/costumers/dto/create-costumer.dto.ts
+++ src/costumers/dto/create-costumer.dto.ts
@@ -1,1 +1,52 @@
-export class CreateCostumerDto {}
+import { Prisma } from '.prisma/client';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNotEmptyObject,
+  IsObject,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
+
+enum CostumerDocumentType {
+  cpf = 'CPF',
+  cnpj = 'CNPJ',
+}
+export type CostumerDocument = {
+  number: string;
+  type: CostumerDocumentType;
+  country: string;
+};
+
+export type CostumerAdderss = {
+  country: string;
+  state: string;
+  city: string;
+  district: string;
+  zipCode: string;
+  street: string;
+  streetNumber: string;
+  complement: string;
+};
+export class CreateCostumerDto implements Prisma.CostumersCreateInput {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsNotEmpty()
+  @IsPhoneNumber('BR')
+  phoneNumber: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsObject()
+  @IsNotEmptyObject()
+  document: CostumerDocument;
+
+  @IsObject()
+  @IsNotEmptyObject()
+  address: CostumerAdderss;
+  prodileId: string;
+}
